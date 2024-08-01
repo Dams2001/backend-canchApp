7f07636975a6aef5badc95cbe172ce34db64cf5d
Agustin <agustinmaldonado20@gmail.com>
2024-08-01 14:27:12 -0300

    El cielo es el limite

---
+++ app/Http/Controllers/ServicioDelSistemaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicioDelSistemaController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\DB;
+
+
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\ReservaController;
+
+Route::resource('usuarios', UsuarioController::class);
+Route::resource('reservas', ReservaController::class);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Conexión a la base de datos exitosa.';
+    } catch (\Exception $e) {
+        return 'No se pudo conectar a la base de datos: ' . $e->getMessage();
+    }
+});
+
+
+Route::resource('usuarios', UsuarioController::class);
+Route::resource('reservas', ReservaController::class);
+
+
