6c3d8bf5c1c32a09c8666088559413c6dc29e446
Agustin <agustinmaldonado20@gmail.com>
2024-08-02 17:52:07 -0300

    Agrego login

---
+++ resources/views/sesion.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es-AR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+    />
+    <title>CANCHAPP</title>
+
+    <link rel="stylesheet" href="./style/index.css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <section class="sesion-canchapp">
+      <div class="canchapp-img">
+        <img src="assets/img/CANCHAPP_logo_trans.png" alt="" />
+      </div>
+
+      <div class="sesion-form-canchapp">
+        <form action="">
+          <div>
+            <label for="">Email</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Su correo electronico"
+            />
+          </div>
+
+          <div>
+            <label for="">Contraseña</label>
+            <input type="password" name="" id="" placeholder="Contraseña" />
+          </div>
+
+          <div>
+            <input
+              class="sesion-canchapp-inicio"
+              type="submit"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
+
+      <div class="button-google-sesion">
+        <button><i class="fab fa-google"></i>Iniciar Sesión con Google</button>
+      </div>
+
+      <div class="registrate-canchapp-inicio">
+        <a href="#">¿Aún no eres miembro?<strong> Regístrate aquí</strong> </a>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <div class="colum-1">
+          <h2>Help Desk 24h/7</h2>
+          <p>
+            Lorem ipsum dolor sit amet enim. Suspendisse a pellentesque dui, non
+            felis.
+          </p>
+          <p class="number">
+            <span style="color: #0fe5d8">+61</span> (0) 383 766 284
+          </p>
+          <p>noreply@envato.com</p>
+        </div>
+
+        <div class="reviews">
+          <h2>Download our app</h2>
+          <h2>163 reviews</h2>
+          <p>
+            9,6/10
+            <img src="./assets/img/app5-slider-pic3.png" alt="" />
+          </p>
+
+          <div class="images">
+            <img src="./assets/img/PlayStore-260x77.png" alt="Play Store" />
+            <img src="./assets/img/AppStore.png" alt="App Store" />
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="./javascript/index.js"></script>
+  </body>
+</html>
