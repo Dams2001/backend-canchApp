a9ec2cddb5b471c31c0e96089af135f8a867ba1d
Agustin <agustinmaldonado20@gmail.com>
2024-08-02 19:21:01 -0300

    agrego css y mod index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -37,9 +37,9 @@
           >Hace tu Reserva <i class="fas fa-angle-right"></i
         ></a>
 
-      <!-- <a href="{{ url('/login') }}" class="btn-sesion"><i class="fas fa-user"></i></a> -->
+      <a href="{{ url('/login') }}" class="btn-sesion"><i class="fas fa-user"></i></a>
 
-        <a href="/login" class="btn-sesion"><i class="fas fa-user"></i></a>
+        <!-- <a href="/login" class="btn-sesion"><i class="fas fa-user"></i></a> -->
       </div>
     </header>
 
